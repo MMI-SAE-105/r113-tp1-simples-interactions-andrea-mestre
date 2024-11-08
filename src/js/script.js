f0a37188d48b358510d4e1b57dcdac0424b6944a
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-11-08 14:17:51 +0100

    Exercice JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,21 @@
 // TODO
+
+//ex1
 const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
 
 aMettreEnRouge.style.color = "red";
 
+//ex2
 const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 
 enRougeSuiteAClick.addEventListener("click", (evt) => {
     enRougeSuiteAClick.style.color = "red";
 })
 
+//ex3
 const collH2 = document.querySelectorAll("h2");
 
 collH2.forEach((elm) => {
     elm.addEventListener("click", (evt) => {
         evt.target.style.color = "red";
-    })
-})
+    })})
